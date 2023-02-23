3b186f3f61c5ea5e027846d705e66a2335df6b3d
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-02-23 09:32:43 +0100

    add of documentation for Auth Route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,17 +4,79 @@ var authCtrl = require('../controllers/auth.ctrl');
 var verifySignup = require('../middlewares/verifySignup');
 
 
-//Road to signup
-// Can create a new user
+/**
+ * @swagger
+ * /api/auth/signup:
+ *  post:
+ *      summary: Allows to a new user to sign up and so create an account.
+ *      security: []
+ *      tags: [Auth]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/User'
+ *      responses:
+ *              201:
+ *                  description: Succès.
+ *              400:
+ *                  description: Erreur dans la requête.
+ *              500:
+ *                  description: Erreur serveur.
+ */
 // Here we use the middleware verifySignup to check if the email is already used and if the role is correct
 router.post('/signup', [verifySignup.checkDuplicateEmail, verifySignup.checkRolesExisted], authCtrl.signup);
 
-//Road to signin
-// Can connect a user
+/**
+ * @swagger
+ * /api/auth/signin:
+ *  post:
+ *     summary: Allows to a user to sign in and so connect to his account.
+ *     security: []
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *            schema:
+ *               type: object
+ *               properties:
+ *                email:
+ *                  type: string
+ *                  description: The email of the user.
+ *                password:
+ *                  type: string
+ *                  description: The password of the user.
+ *     responses:
+ *              200:
+ *                  description: Succès.
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User'
+ *              400:
+ *                  description: Erreur dans la requête.
+ *              500:
+ *                  description: Erreur serveur.
+ */
 router.post('/signin', authCtrl.signin);
 
-//Road to signout
-// Can disconnect a user
+/**
+ * @swagger
+ * /api/auth/signout:
+ *  post:
+*     summary: Allows to a user to sign out and so disconnect from his account.
+*     security: []
+*     tags: [Auth]
+*     responses:
+*       200:
+*           description: Succès.
+*       400:
+*           description: Erreur dans la requête.
+*       500:
+*           description: Erreur serveur.
+ */
 router.post('/signout', authCtrl.signout);
 
 
