3d9872278ac7b5c37f5444acb63edf67d847b51a
HAKEMI Ayoub <ayoub.hakemi@igtp3-pc12.isim.intra>
2023-02-23 20:05:50 +0100

    get the zone by jeu id

--- src/controllers/zone.ctrl.js
+++ src/controllers/zone.ctrl.js
@@ -72,7 +72,7 @@ exports.getZoneByJeuId = function(req, res){
     Zone.findOne({jeux : req.params.id}).populate('jeux')
         .then(zone => res.status(200).json(zone))
         .catch(error => {
-            console.log(error);
+            console.log("t dans la merde",error);
             res.status(400).json({error});
         });
 
