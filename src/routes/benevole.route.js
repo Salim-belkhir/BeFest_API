0779941437d2836d3b960b4a38754f28de9cf317
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-02-23 10:36:02 +0100

    Add of documentation for Benevole road

--- src/routes/benevole.route.js
+++ src/routes/benevole.route.js
@@ -4,24 +4,107 @@ const BenevoleCtrl = require('../controllers/benevole.ctrl');
 const auth = require('../middlewares/auth');
 
 
-// Path : api/benevoles
-// Get all Benevoles
+/**
+ * @swagger
+ * /api/benevoles:
+ *  get:
+ *      summary: Get all the benevoles that exists.
+ *      tags: [Benevole]
+ *      security: 
+ *         - ApiKeyAuth: [admin]
+ *      description: This route allows to get all the benevoles of the API.
+ *      responses:
+ *          200:
+ *              description: Success.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/User'
+ *          400:
+ *              description: Bad request.
+ */
 router.get('/', auth.verifyToken, auth.isAdmin, BenevoleCtrl.getAllBenevoles);
 
-// Path : api/benevoles/:id
-// Get a Benevole by its id
+/**
+ * @swagger
+ * /api/benevoles/{id}:
+ *  get:
+ *      summary: Get a benevole by its id.
+ *      tags: [Benevole]
+ *      security:
+ *          - ApiKeyAuth: [admin]
+ *      description: This route allows to get a benevole by its id.
+ *      parameters:
+ *          - in: path
+ *              name: id
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The benevole id
+ *      responses:
+ *          200:
+ *              description: Success.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: Ressource not found.
+ */
 router.get('/:id',auth.verifyToken, auth.isAdmin,BenevoleCtrl.getBenevoleById);
 
 // Path : api/benevoles
 // Create a new Benevole
 //router.post('/', BenevoleCtrl.createBenevole);
 
-// Path : api/benevoles/:id
-// Update a Benevole
+/**
+ * @swagger
+ * /api/benevoles:
+ *  put:
+ *      summary: Update the informations of a benevole.
+ *      tags: [Benevole]
+ *      security:
+ *          - ApiKeyAuth: [admin]
+ *      description: This route allows to create a new benevole.
+ *      requestBody:
+ *          description: The benevole to create.
+ *          required: true
+ *          content:
+ *             application/json:
+ *                 schema:
+ *                    $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: Success.
+ *          400:
+ *              description: Bad request.
+ */
 router.put('/:id', auth.verifyToken, auth.isAdmin ,BenevoleCtrl.updateBenevole);
 
-// Path : api/benevoles/:id
-// Delete a Benevole
+/**
+ * @swagger
+ *  /api/benevoles/{id}:
+ *     delete:
+ *          summary: Delete a benevole.
+ *          tags: [Benevole]
+ *          security:
+ *              - ApiKeyAuth: [admin]
+ *          description: This route allows to delete a benevole.
+ *          parameters:
+ *                - in: path
+ *                  name: id
+ *                  schema:
+ *                      type: string
+ *                  required: true
+ *                  description: The benevole id
+ *          responses:
+ *              200:
+ *                  description: Success.
+ *              400:
+ *                  description: Bad request.
+ */
 router.delete('/:id', auth.verifyToken, auth.isAdmin,BenevoleCtrl.deleteBenevole);
 
 
