c75053c49f086faf0b6fb451a498b464b7d470ae
HAKEMI Ayoub <ayoub.hakemi@igtp3-pc12.isim.intra>
2023-02-23 19:32:44 +0100

    i dont know why its not working

--- src/controllers/zone.ctrl.js
+++ src/controllers/zone.ctrl.js
@@ -70,7 +70,7 @@ exports.deleteZone = function(req, res){
 // Get the zone of a jeu by the id of the jeu
 exports.getZoneByJeuId = function(req, res){
     Zone.findOne({jeux : req.params.id}).populate('jeux')
-        .then(zone => res.status(200).json("bibo"))
+        .then(zone => res.status(200).json(zone))
         .catch(error => {
             console.log(error);
             res.status(400).json({error});
